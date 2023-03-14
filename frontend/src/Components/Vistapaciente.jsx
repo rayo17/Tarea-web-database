bce10926bfe68e8f98597605632faf8858655bf6
rayo17 <drayo.dard.16@gmail.com>
2023-03-13 23:28:13 -0600

    creacion del componente Cuenta para crear la cuente del paciente

--- frontend/src/Components/Vistapaciente.jsx
+++ frontend/src/Components/Vistapaciente.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Gestion from './Gestion'
 import Menu from './Menu'
+import '../style/Vistapaciente.css'
 function Vistapaciente () {
   return (
     <div>
         <Menu/>
-        <div>
+        <div className='container'>
         <Gestion titulo='Crear cuenta'/>
         <Gestion titulo='Reservacion'/>
         <Gestion titulo='Historial'/>
