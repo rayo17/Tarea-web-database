08b9307c0e0d44ab3c4a0cc3f45aa37e503f967c
joanjuz <joanjuz@gmail.com>
2023-03-21 16:43:28 -0600

    PostPaciente!

--- API_Rest/API_Rest/Controllers/PacienteController.cs
+++ API_Rest/API_Rest/Controllers/PacienteController.cs
@@ -32,13 +32,64 @@ namespace API_Rest.Controllers
             //Query de SELECT del view VIEWPACIENTE para obtener los datos necesarios para mostrar todos los pacientes
             string query =
                 "SELECT "
-                + "* "
+                + "nombre, "
+                + "cedula "
                 + "FROM "
                 + "Paciente"
                 + ";";
             
             //Retorna todos los objetos obtenidos
             return await _context.vpaciente.FromSqlRaw(query).ToListAsync();
+        }
+        /*
+         * GET: "api/GetPacienteC/cedula"
+         * Obtiene solo el paciente con la cedula indicado
+         */
+        [Route("api/Paciente/{cedula}")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<vPaciente>>> GetPacienteC(int cedula)
+        {
+            //Query de SELECT del view VIEWPACIENTE para obtener los datos necesarios para mostrar todos los pacientes
+            string query =
+                "SELECT "
+                + "* "
+                + "FROM "
+                + "Paciente "
+                + "WHERE "
+                + "cedula = " + cedula.ToString()
+                + ";";
+
+            //Retorna todos los objetos obtenidos del view de historial_clinico
+            return await _context.vpaciente.FromSqlRaw(query).ToListAsync();
+        }
+        /*
+         * POST: "api/PostPaciente"
+         * Agregar un paciente nuevo a la base de datos con la informacion que se indica en el Form.
+         */
+        [Route("api/PostPaciente")]
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<vPaciente>>> PostPaciente([FromBody] Paciente paciente)
+        {
+            /*_context.paciente.Add(paciente);
+            await _context.SaveChangesAsync();*/
+
+            string query = "INSERT INTO Paciente("
+                           +"Nombre, "
+                           + "Primer_apellido, " +"Segundo_apellido, " 
+                           +"Cedula, " + "Fecha_nacimiento) "
+                           + "VALUES (" 
+                           + $"'{paciente.Nombre}', '{paciente.Primer_apellido}', "
+                           + $"'{paciente.Segundo_apellido}', {paciente.Cedula}, "
+                           + $"'{paciente.Fecha_nacimiento.ToString("yyyy-MM-dd")}'"
+                           + ");";
+            
+            Console.WriteLine(query);
+
+            await _context.Database.ExecuteSqlRawAsync(query);
+
+            Task<ActionResult<IEnumerable<vPaciente>>> paciente_wid = GetPacienteC(paciente.Cedula);
+
+            return await paciente_wid; //CreatedAtAction("GetPaciente", new { id = paciente.cedula }, paciente);
         }
 
     }
