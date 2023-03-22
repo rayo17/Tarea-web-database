99a3b185d4842730e9b9def34498c8b36fffcf76
joanjuz <joanjuz@gmail.com>
2023-03-21 23:57:54 -0600

    .

--- API_Rest/API_Rest/Models/Views/vPacienteCompleto.cs
+++ API_Rest/API_Rest/Models/Views/vPacienteCompleto.cs
@@ -7,8 +7,8 @@ public class vPacienteCompleto
     [Key]
     public int cedula { get; set; }
     public string nombre { get; set; }
-    public List<TelefonosPaciente> telefono { get; set; }
-    public List<DireccionPaciente> direcciones { get; set; }
-    public List<Paciente_Patologia> patologia { get; set; }
+    public string telefono { get; set; }
+    public string direccion { get; set; }
+    public string patologia { get; set; }
     //public string tratamiento { get; set; }
 }
