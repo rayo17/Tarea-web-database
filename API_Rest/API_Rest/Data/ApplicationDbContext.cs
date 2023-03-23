5d91d54b53781497307dd1b72cd855d9e8b8ff50
joanjuz <joanjuz@gmail.com>
2023-03-23 00:47:50 -0600

    Patologia listo

--- API_Rest/API_Rest/Data/ApplicationDbContext.cs
+++ API_Rest/API_Rest/Data/ApplicationDbContext.cs
@@ -26,6 +26,7 @@ namespace API_Rest.Data
         public DbSet<Paciente_Direcciones> Paciente_Direcciones { get; set; }
         public DbSet<Paciente_Telefonos> Paciente_Telefonos { get; set; }
         public DbSet<Historial> Historial { get; set; }
+        public DbSet<Patologia> Patologia { get; set; }
         //public DbSet<Direcciones_paciente> Direcciones_paciente { get; set; }
     }
 }
