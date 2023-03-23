6c5e1fcb5b7ec9705d042fce8513bbbfdce1cfbd
Ignacio Grane <ignaciograner@gmail.com>
2023-03-22 21:01:25 -0600

    Get by id and delete

--- API_Rest/API_Rest/Controllers/PacienteController.cs
+++ API_Rest/API_Rest/Controllers/PacienteController.cs
@@ -2,6 +2,7 @@ using API_Rest.Data;
 using API_Rest.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
 
 namespace DetailTEC_API.Controllers
 {
@@ -20,6 +21,27 @@ namespace DetailTEC_API.Controllers
         public IEnumerable<Paciente> Get()
         {
             return context.Paciente.ToList();
+        }
+
+        // GET: api/<PacienteController>/cedula
+        [HttpGet("{cedula}")]
+        public Paciente Get(string cedula)
+        {
+            return context.Paciente.FirstOrDefault(p => p.Cedula == cedula);
+        }
+
+        [HttpDelete("{cedula}")]
+        public ActionResult Delete(string cedula)
+        {
+            var paciente = context.Paciente.FirstOrDefault(p => p.Cedula == cedula);
+            if (paciente != null)
+            {
+                
+                context.Paciente.Remove(paciente);
+                context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
