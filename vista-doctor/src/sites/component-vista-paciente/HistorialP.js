fc43132a5026f3324b7dc1550b7871e38c872c87
rayo17 <drayo.dard.16@gmail.com>
2023-03-25 20:28:48 -0600

    cambios en la vista paciente

--- vista-doctor/src/sites/component-vista-paciente/HistorialP.js
+++ vista-doctor/src/sites/component-vista-paciente/HistorialP.js
@@ -1,43 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import './historial.css'
 
 function HistorialP() {
     let [usuario, setusuarios] = useState([])
+    const [visible, setvible] = useState('null')
+    const [cedula, setcedula] = useState('')
+    const changeCedula = (e) => {
+        setcedula(e.target.value)
+    }
 
-
-    useEffect(()=>{
-        cargar_historial()
-    })
+    /* useEffect(()=>{
+         cargar_historial()
+     })*/
     const cargar_historial = async () => {
         const resp = await axios.get('https://localhost:44362/api/Historial')
         setusuarios(resp.data)
+        setvible('historial')
 
     }
-   
+
     return (
         <div>
-            {
-                usuario.map((usuario, index) => {
-                    return (
-                        <div key={index}>
-                            <div>
-                                <label>Fecha</label>
-                                <input value={usuario.fecha} />
-                            </div>
-                            <div>
-                                <label>Procedimiento</label>
-                                <input value={usuario.procedimiento} />
-                            </div>
-                            <div>
-                                <label>Tratamiento</label>
-                                <input value={usuario.tratamiento} />
-                            </div>
+            <div>
+                <form onSubmit={cargar_historial}>
+                    <label>Ingresa tu cedula para ver tu historial clinico</label>
+                    <input value={cedula} onChange={changeCedula} />
+                </form>
 
-                        </div>
-                    )
-                })
+            </div>
+            <div className={visible}>
+                {
+                    usuario.map((usuario, index) => {
+                        return (
+                            <div key={index}>
+                                <div>
+                                    <label>Fecha</label>
+                                    <input value={usuario.fecha} />
+                                </div>
+                                <div>
+                                    <label>Procedimiento</label>
+                                    <input value={usuario.procedimiento} />
+                                </div>
+                                <div>
+                                    <label>Tratamiento</label>
+                                    <input value={usuario.tratamiento} />
+                                </div>
 
-            }
+                            </div>
+                        )
+                    })
+
+                }
+
+            </div>
 
 
         </div>
